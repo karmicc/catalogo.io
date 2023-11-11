34ef68d6474e6218a0fad9735d591089f94203cc
Víctor Hugo López Pérez <smackpanther@hotmail.com>
2023-11-11 13:11:49 -0600

    se configura datatables para mostrar solo una columna

--- static/js/categoria1.js
+++ static/js/categoria1.js
@@ -4,20 +4,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Función para llenar la tabla con los productos
   function llenarTabla() {
-    let tabla = $("#tablaProductos").DataTable({
-      responsive: true // Hace la tabla responsiva
-      // Puedes agregar otras opciones de configuración aquí
+    let tabla = $('#tablaProductos').DataTable({
+      responsive: true,
+      columnDefs: [
+        { responsivePriority: 1, targets: 0}, // Prioridad alta en la primera columna (Imagen)
+        { responsivePriority: 2, targets: [1, 2], visible: false }  // Prioridad baja en la tercera columna (Precio)
+      ],
     });
+
+
     tabla.clear(); // Limpiar la tabla antes de rellenar
 
     const filasHTML = productos.map((producto) => [
-      `<img src="./../.${producto.imagen}" alt="${producto.nombre}">`,
-      producto.nombre,
-      producto.precio,
-    ]);
+        `<div class="text-center"><img src="./../.${producto.imagen}" alt="${producto.nombre}" class="img-fluid"></div>`,
+        producto.nombre,
+        producto.precio,
+      ]);
 
     // Añadir filas a la tabla
     tabla.rows.add(filasHTML).draw();
+    // Configurar el evento click para expandir filas
+    $('#tablaProductos tbody').on('click', 'tr', function () {
+        const row = tabla.row(this);
+        mostrarDetalles(productos[row.index()])
+      });
   }
 
   // Llama a la función para llenar la tabla cuando el DOM esté completamente cargado
